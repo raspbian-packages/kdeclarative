b409f5b712b8fa1c16e293ef074f3b57ffb0a5f8
Maximiliano Curia <maxy@debian.org>
2019-09-20 18:12:57 +0100

    Import kdeclarative_5.62.0.orig.tar.xz
    [dgit import orig kdeclarative_5.62.0.orig.tar.xz]

---
+++ src/qmlcontrols/kquickcontrolsaddons/kquickcontrolsaddonsplugin.cpp
@@ -0,0 +1,77 @@
+/*
+ *   Copyright 2009 by Alan Alpert <alan.alpert@nokia.com>
+ *   Copyright 2010 by Ménard Alexis <menard@kde.org>
+ *   Copyright 2010 by Marco Martin <mart@kde.org>
+
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "kquickcontrolsaddonsplugin.h"
+#include "config-kquickcontrolsaddons.h"
+
+
+#include "qpixmapitem.h"
+#include "qimageitem.h"
+#include "qiconitem.h"
+#include "mouseeventlistener.h"
+#include "columnproxymodel.h"
+#include "clipboard.h"
+#include "fallbacktaphandler.h"
+#include "mimedatabase.h"
+#include "kcmshell.h"
+#include "icondialog.h"
+#include "eventgenerator.h"
+
+#if HAVE_EPOXY
+#include "plotter.h"
+#endif
+
+static QObject *kcmshell_singleton_provider(QQmlEngine *engine, QJSEngine *scriptEngine)
+{
+    Q_UNUSED(engine);
+    Q_UNUSED(scriptEngine);
+
+    return new KCMShell();
+}
+
+void KQuickControlsAddonsPlugin::registerTypes(const char *uri)
+{
+    Q_ASSERT(QString::fromLatin1(uri) == QLatin1String("org.kde.kquickcontrolsaddons"));
+
+    qmlRegisterType<QPixmapItem>(uri, 2, 0, "QPixmapItem");
+    qmlRegisterType<QImageItem>(uri, 2, 0, "QImageItem");
+    qmlRegisterType<QIconItem>(uri, 2, 0, "QIconItem");
+    qmlRegisterType<MouseEventListener>(uri, 2, 0, "MouseEventListener");
+    qmlRegisterType<ColumnProxyModel>(uri, 2, 0, "ColumnProxyModel");
+    qmlRegisterType<Clipboard>(uri, 2, 0, "Clipboard");
+    qmlRegisterType<MimeDatabase>(uri, 2, 0, "MimeDatabase");
+    qmlRegisterSingletonType<KCMShell>(uri, 2, 0, "KCMShell", kcmshell_singleton_provider);
+    qmlRegisterType<IconDialog>(uri, 2, 0, "IconDialog");
+    qmlRegisterType<EventGenerator>(uri, 2, 0, "EventGenerator");
+    qmlRegisterUncreatableType<FallbackTapHandlerMouseEvent>(uri, 2, 1, "FallbackTapHandlerMouseEvent", QStringLiteral("Cannot create items of type FallbackTapHandlerMouseEvent"));
+    qmlRegisterType<FallbackTapHandler>(uri, 2, 1, "FallbackTapHandler");
+
+#if HAVE_EPOXY
+    qmlRegisterType<PlotData>(uri, 2, 0, "PlotData");
+    qmlRegisterType<Plotter>(uri, 2, 0, "Plotter");
+#endif
+
+    qmlRegisterType<QAbstractItemModel>();
+    qRegisterMetaType<QModelIndex>("QModelIndex");
+}
+
+
+
---
+++ src/quickaddons/configmodule.cpp
@@ -0,0 +1,445 @@
+/*
+   This file is part of the KDE libraries
+
+   Copyright (c) 1999 Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
+   Copyright (c) 2001 Michael Goffioul <kdeprint@swing.be>
+   Copyright (C) 2004 Frans Englich <frans.englich@telia.com>
+   Copyright (C) 2009 Dario Freddi <drf@kde.org>
+   Copyright (C) 2015 Marco Martin <mart@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+
+*/
+
+#include "configmodule.h"
+
+#include <QDebug>
+#include <QUrl>
+#include <QQmlEngine>
+#include <QQmlContext>
+#include <QQuickItem>
+#include <QQmlEngine>
+
+#include <kaboutdata.h>
+#include <klocalizedstring.h>
+#include <kdeclarative/qmlobject.h>
+#include <kdeclarative/qmlobjectsharedengine.h>
+
+#include <KPackage/Package>
+#include <KPackage/PackageLoader>
+
+namespace KQuickAddons {
+
+class ConfigModulePrivate
+{
+public:
+    ConfigModulePrivate(ConfigModule *module):
+        _q(module),
+        _qmlObject(nullptr),
+        _buttons(ConfigModule::Help | ConfigModule::Default | ConfigModule::Apply),
+        _about(nullptr),
+        _useRootOnlyMessage(false),
+        _needsAuthorization(false),
+        _needsSave(false)
+    {
+    }
+
+    void authStatusChanged(int status);
+
+    ConfigModule *_q;
+    KDeclarative::QmlObject *_qmlObject;
+    ConfigModule::Buttons _buttons;
+    const KAboutData *_about;
+    QString _rootOnlyMessage;
+    QString _quickHelp;
+    QList<QQuickItem *> subPages;
+    int _columnWidth = -1;
+    int currentIndex = 0;
+    bool _useRootOnlyMessage : 1;
+
+    bool _needsAuthorization : 1;
+    bool _needsSave  :1;
+    QString _authActionName;
+
+    static QHash<QObject *, ConfigModule *> s_rootObjects;
+};
+
+QHash<QObject *, ConfigModule *> ConfigModulePrivate::s_rootObjects = QHash<QObject *, ConfigModule *>();
+
+
+ConfigModule::ConfigModule(const KAboutData *aboutData, QObject *parent, const QVariantList &)
+    : QObject(parent), d(new ConfigModulePrivate(this))
+{
+    setAboutData(aboutData);
+}
+
+ConfigModule::ConfigModule(const KPluginMetaData &metaData, QObject *parent, const QVariantList &)
+    : QObject(parent), d(new ConfigModulePrivate(this))
+{
+    KAboutData *aboutData = new KAboutData(metaData.pluginId(), metaData.name(), metaData.version(), metaData.description(), KAboutLicense::byKeyword(metaData.license()).key());
+
+    const auto authors = metaData.authors();
+    for (auto& author : authors) {
+        aboutData->addAuthor(author.name(), author.task(), author.emailAddress(), author.webAddress(), author.ocsUsername());
+    }
+    setAboutData(aboutData);
+}
+
+ConfigModule::ConfigModule(QObject *parent, const QVariantList &)
+    : QObject(parent), d(new ConfigModulePrivate(this))
+{
+}
+
+ConfigModule::~ConfigModule()
+{
+    //in case mainUi was never called
+    if (d->_qmlObject) {
+        ConfigModulePrivate::s_rootObjects.remove(d->_qmlObject->rootContext());
+    }
+
+    delete d->_qmlObject;
+    delete d->_about;
+    delete d;
+}
+
+
+
+ConfigModule *ConfigModule::qmlAttachedProperties(QObject *object)
+{
+    //at the moment of the attached object creation, the root item is the only one that hasn't a parent
+    //only way to avoid creation of this attached for everybody but the root item
+    const QQmlEngine *engine = QtQml::qmlEngine(object);
+    QQmlContext *cont = QQmlEngine::contextForObject(object);
+
+    //Search the qml context that is the "root" for the sharedqmlobject, which
+    //is an ancestor of QQmlEngine::contextForObject(object) and the direct child
+    //of the engine's root context: we can do this assumption on the internals as
+    //we are distributed on the same repo.
+    while (cont->parentContext() && cont->parentContext() != engine->rootContext()) {
+        cont = cont->parentContext();
+    }
+
+    if (!object->parent() && ConfigModulePrivate::s_rootObjects.contains(cont)) {
+        return ConfigModulePrivate::s_rootObjects.value(cont);
+    } else {
+        return nullptr;
+    }
+}
+
+QQuickItem *ConfigModule::mainUi()
+{
+    if (d->_qmlObject) {
+        return qobject_cast<QQuickItem *>(d->_qmlObject->rootObject());
+    }
+
+    // if we have a qml context, hook up to it and use its engine
+    // this ensure that in e.g. Plasma config dialogs that use a different engine
+    // so they can have different QtQuick Controls styles, we don't end up using
+    // the shared engine that is used by the rest of plasma
+
+    QQmlContext *ctx = QQmlEngine::contextForObject(this);
+
+    if (ctx && ctx->engine()) {
+        d->_qmlObject = new KDeclarative::QmlObject(ctx->engine(), ctx, this);
+    } else {
+        d->_qmlObject = new KDeclarative::QmlObjectSharedEngine(this);
+    }
+
+    ConfigModulePrivate::s_rootObjects[d->_qmlObject->rootContext()] = this;
+    d->_qmlObject->setTranslationDomain(aboutData()->componentName());
+    d->_qmlObject->setInitializationDelayed(true);
+
+    KPackage::Package package = KPackage::PackageLoader::self()->loadPackage(QStringLiteral("KPackage/GenericQML"));
+    package.setDefaultPackageRoot(QStringLiteral("kpackage/kcms"));
+    package.setPath(aboutData()->componentName());
+
+    if (!package.isValid()) {
+        qWarning() << "Error loading the module" << aboutData()->componentName() << ": invalid KPackage";
+        return nullptr;
+    }
+
+    if (!package.filePath("mainscript").isEmpty()) {
+        d->_qmlObject->setSource(package.fileUrl("mainscript"));
+        d->_qmlObject->rootContext()->setContextProperty(QStringLiteral("kcm"), this);
+        d->_qmlObject->completeInitialization();
+
+        return qobject_cast<QQuickItem *>(d->_qmlObject->rootObject());
+    } else {
+        qWarning() << "Error loading the module" << aboutData()->componentName() << ": no QML file provided";
+        return nullptr;
+    }
+}
+
+void ConfigModule::push(const QString &fileName, const QVariantMap &propertyMap)
+{
+    //ensure main ui is created
+    if (!mainUi()) {
+        return;
+    }
+
+    //TODO: package as member
+    KPackage::Package package = KPackage::PackageLoader::self()->loadPackage(QStringLiteral("KPackage/GenericQML"));
+    package.setDefaultPackageRoot(QStringLiteral("kpackage/kcms"));
+    package.setPath(aboutData()->componentName());
+
+    QVariantHash propertyHash;
+    for (auto it = propertyMap.begin(), end = propertyMap.end(); it != end; ++it) {
+        propertyHash.insert(it.key(), it.value());
+    }
+
+    QObject *object = d->_qmlObject->createObjectFromSource(QUrl::fromLocalFile(package.filePath("ui", fileName)),
+                                                            d->_qmlObject->rootContext(),
+                                                            propertyHash);
+
+    QQuickItem *item = qobject_cast<QQuickItem *>(object);
+    if (!item) {
+        object->deleteLater();
+        return;
+    }
+
+    d->subPages << item;
+    emit pagePushed(item);
+    emit depthChanged(depth());
+    setCurrentIndex(d->currentIndex + 1);
+}
+
+void ConfigModule::push(QQuickItem *item)
+{
+    //ensure main ui is created
+    if (!mainUi()) {
+        return;
+    }
+
+    d->subPages << item;
+    emit pagePushed(item);
+    emit depthChanged(depth());
+    setCurrentIndex(d->currentIndex + 1);
+}
+
+void ConfigModule::pop()
+{
+    if (d->subPages.isEmpty()) {
+        return;
+    }
+    QQuickItem *page = d->subPages.takeLast();
+    emit pageRemoved();
+    emit depthChanged(depth());
+    page->deleteLater();
+
+    setCurrentIndex(qMin(d->currentIndex, depth() - 1));
+}
+
+ConfigModule::Buttons ConfigModule::buttons() const
+{
+    return d->_buttons;
+}
+
+void ConfigModule::setButtons(const KQuickAddons::ConfigModule::Buttons buttons)
+{
+    if (d->_buttons == buttons) {
+        return;
+    }
+
+    d->_buttons = buttons;
+    emit buttonsChanged();
+}
+
+void ConfigModule::setNeedsAuthorization(bool needsAuth)
+{
+    if (d->_needsAuthorization == needsAuth) {
+        return;
+    }
+
+    d->_needsAuthorization = needsAuth;
+    if (needsAuth && d->_about) {
+        d->_authActionName = QLatin1String("org.kde.kcontrol.") + d->_about->componentName() + QLatin1String(".save");
+        d->_needsAuthorization = true;
+
+    } else {
+        d->_authActionName = QString();
+    }
+
+    emit needsAuthorizationChanged();
+    emit authActionNameChanged();
+}
+
+bool ConfigModule::needsAuthorization() const
+{
+    return d->_needsAuthorization;
+}
+
+QString ConfigModule::name() const
+{
+    return d->_about->displayName();
+}
+
+QString ConfigModule::description() const
+{
+    return d->_about->shortDescription();
+}
+
+int ConfigModule::columnWidth() const
+{
+    return d->_columnWidth;
+}
+
+void ConfigModule::setColumnWidth(int width)
+{
+    if (d->_columnWidth == width) {
+        return;
+    }
+
+    d->_columnWidth = width;
+    emit columnWidthChanged(width);
+}
+
+int ConfigModule::depth() const
+{
+    return d->subPages.count() + 1;
+}
+
+void ConfigModule::setCurrentIndex(int index)
+{
+    if (index < 0 || index > d->subPages.count() || index == d->currentIndex) {
+        return;
+    }
+
+    d->currentIndex = index;
+
+    emit currentIndexChanged(index);
+}
+
+int ConfigModule::currentIndex() const
+{
+    return d->currentIndex;
+}
+
+void ConfigModule::setAuthActionName(const QString &name)
+{
+    if (d->_authActionName == name) {
+        return;
+    }
+
+    d->_authActionName = name;
+    d->_needsAuthorization = true;
+
+    emit needsAuthorizationChanged();
+    emit authActionNameChanged();
+}
+
+QString ConfigModule::authActionName() const
+{
+    return d->_authActionName;
+}
+
+QQmlEngine *ConfigModule::engine() const
+{
+    return d->_qmlObject->engine();
+}
+
+void ConfigModule::load()
+{
+    setNeedsSave(false);
+}
+
+void ConfigModule::save()
+{
+    setNeedsSave(false);
+}
+
+void ConfigModule::defaults()
+{
+}
+
+const KAboutData *ConfigModule::aboutData() const
+{
+    return d->_about;
+}
+
+void ConfigModule::setAboutData(const KAboutData *about)
+{
+    if (about != d->_about) {
+        delete d->_about;
+        d->_about = about;
+    }
+}
+
+void ConfigModule::setRootOnlyMessage(const QString &message)
+{
+    if (d->_rootOnlyMessage == message) {
+        return;
+    }
+
+    d->_rootOnlyMessage = message;
+    emit rootOnlyMessageChanged();
+}
+
+QString ConfigModule::rootOnlyMessage() const
+{
+    return d->_rootOnlyMessage;
+}
+
+void ConfigModule::setUseRootOnlyMessage(bool on)
+{
+    if (d->_useRootOnlyMessage == on) {
+        return;
+    }
+
+    d->_useRootOnlyMessage = on;
+
+    emit useRootOnlyMessageChanged();
+}
+
+bool ConfigModule::useRootOnlyMessage() const
+{
+    return d->_useRootOnlyMessage;
+}
+
+void ConfigModule::setQuickHelp(const QString &help)
+{
+    if (d->_quickHelp == help) {
+        return;
+    }
+
+    d->_quickHelp = help;
+
+    emit quickHelpChanged();
+}
+
+QString ConfigModule::quickHelp() const
+{
+    return d->_quickHelp;
+}
+
+void ConfigModule::setNeedsSave(bool needs)
+{
+    if (needs == d->_needsSave) {
+        return;
+    }
+
+    d->_needsSave = needs;
+    emit needsSaveChanged();
+}
+
+bool ConfigModule::needsSave()
+{
+    return d->_needsSave;
+}
+
+}
+
+#include "moc_configmodule.cpp"
+
