8a48e362408486ec2fee92e4be1b1375ebe8a49b
Aurélien COUDERC <coucouf@debian.org>
2025-04-13 00:43:06 +0200

    Import kdeclarative_5.116.0.orig.tar.xz
    [dgit import orig kdeclarative_5.116.0.orig.tar.xz]

---
+++ src/qmlcontrols/kconfig/kconfigplugin.cpp
@@ -0,0 +1,36 @@
+/*
+    SPDX-FileCopyrightText: 2016 Kai Uwe Broulik <kde@privat.broulik.de>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
+#include "kconfigplugin.h"
+
+#include <KAuthorized>
+#include <QQmlEngine>
+
+#include "kauthorizedproxy.h"
+
+void KConfigPlugin::registerTypes(const char *uri)
+{
+    Q_ASSERT(QString::fromLatin1(uri) == QLatin1String("org.kde.kconfig"));
+
+    qmlRegisterSingletonType<KAuthorizedProxy>(uri, 1, 0, "KAuthorized", [](QQmlEngine *, QJSEngine *) {
+        auto proxy = new KAuthorizedProxy();
+
+        // Register the enum values dynamically, just like one can access them in from KAuthorized namespace in C++
+        const QMetaEnum restriction = QMetaEnum::fromType<KAuthorized::GenericRestriction>();
+        for (int i = 0, count = restriction.keyCount(); i < count; ++i) {
+            const auto key = restriction.key(i);
+            proxy->setProperty(key, QString::fromLatin1(key).toLower());
+        }
+        const QMetaEnum action = QMetaEnum::fromType<KAuthorized::GenericAction>();
+        for (int i = 0, count = action.keyCount(); i < count; ++i) {
+            const auto key = action.key(i);
+            proxy->setProperty(key, QString::fromLatin1(key).toLower());
+        }
+        return proxy;
+    });
+}
+
+#include "moc_kconfigplugin.cpp"
---
+++ src/qmlcontrols/kioplugin/kioplugin.cpp
@@ -0,0 +1,20 @@
+/*
+    SPDX-FileCopyrightText: 2014 Antonis Tsiapaliokas <antonis.tsiapaliokas@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
+#include "kioplugin.h"
+
+#include <QQmlEngine>
+
+#include "krunproxy.h"
+
+void KioPlugin::registerTypes(const char *uri)
+{
+    Q_ASSERT(QString::fromLatin1(uri) == QLatin1String("org.kde.kio"));
+
+    qmlRegisterType<KRunProxy>(uri, 1, 0, "KRun");
+}
+
+#include "moc_kioplugin.cpp"
